02c71c7a2ce6e6db8b3146a94b9da1145f27baa9
Chidhvilas Kacham <kachamchidhvilas@gmail.com>
2025-05-05 03:23:16 -0400

    added hosted

---
+++ LLM_hosted_gemini/generate_dockerfile_gemini.py
@@ -0,0 +1,27 @@
+import google.generativeai as genai
+import os
+
+os.environ["GOOGLE_API_KEY"] = "AIzaSyA8tJvQqRbU0yMgoyuKllCorVxZWa_Rrsc"
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model = genai.GenerativeModel('gemini-1.5-pro')
+
+PROMPT = """
+ONLY generate ideal Dockerfile for {language} with best practices. Just share the Dockerfile without any explanation between two lines to make copying dockerfile easy.
+Include:
+- Base image
+- Installing dependencies
+- Setting working directory
+- Adding source code
+- Running the application
+"""
+
+def generate_dockerfile(language):
+    response = model.generate_content(PROMPT)
+    return response.text
+
+if __name__ == "__main__":
+    language = input("Enter a Programming Language:")
+    print(generate_dockerfile(language))
+
+
